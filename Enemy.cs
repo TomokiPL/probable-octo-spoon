c543c3261c8c799d4d41556286ab13582e8d3a28
TomokiPL <54235053+TomokiPL@users.noreply.github.com>
2019-08-18 14:09:04 +0200

    Add files via upload

--- Enemy.cs
+++ Enemy.cs
@@ -5,53 +5,92 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
 
 
-    //Kierunek w jakim wróg się porusza oraz szybkość
-    public float zwrot;
+       
+    public int turn; //Turn 1=right//////-1=left
+    public float defaultSpeed;  //Default movment speed
+    public float rageSpeed; //Speed when see Player
+    private float currentSpeed; //Current movment speed
+    
 
     private Rigidbody2D body;
     private SpriteRenderer sprite;
 
-    //Obiekty wykrywajace przeszkody
-    public GameObject wallDetector;
-    public GameObject groundDetector;
-    public GameObject jumpDetector;
-
-    //Skrypty do detektorów
-   // Detectors groundDetectorScript;
-
-
+    public LayerMask wallLayer;
+    public LayerMask playerLayer;
 
     // Use this for initialization
     void Start () {
         body = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
-       // Detectors groundDetectorScript = groundDetector.GetComponent<Detectors>();
+        currentSpeed = defaultSpeed;
+
     }
 	
 	// Update is called once per frame
 	void Update () {
-        //TEST RAYCASTING
-        Vector3 pos = transform.position;
-        Vector3 newpos = transform.position + new Vector3(0, 1);
-        print(newpos);
-        Debug.DrawLine(pos, newpos, Color.red, 2, true);
-        //zwrot = zwrot * Time.deltaTime * speed;
-        if (groundDetector.GetComponent<Detectors>().GetColide() != true)
-        {
-            this.transform.localScale = new Vector3(this.transform.localScale.x*-1, this.transform.localScale.y);
-            //Odwraca kierunek ruchu
-            zwrot *= -1;
-        }
-        else if(wallDetector.GetComponent<Detectors>().GetColide() == true)
-        {
-            this.transform.localScale = new Vector3(this.transform.localScale.x * -1, this.transform.localScale.y);
-            //Odwraca kierunek ruchu
-            zwrot *= -1;
-        }
 
-            
-            
-        body.velocity = new Vector2(zwrot, body.velocity.y);
+        NearWall();  
+        SeePlayer();
+        OnGround();
+        body.velocity = new Vector2(turn*currentSpeed, body.velocity.y);
+    }
+
+
+    //Check wall hit, if yes turn around
+    private void NearWall()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = new Vector2(0.3f*turn, 0);
+        float distance = 0.3f;
+        Debug.DrawRay(position, direction, Color.green);
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, wallLayer);
+        if (hit.collider != null)
+        {
+            turn *= -1;
+            return;
+        }
+        return;
+    }
+
+    //Check ground hit, if no turn around
+    private void OnGround()
+    {
+        {
+            Vector2 position = transform.position;
+            Vector2 direction = new Vector2(turn, -1);
+            float distance = 1f;
+            Debug.DrawRay(position, direction, Color.green);
+            RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, wallLayer);
+            if (hit.collider == null)
+            {
+                turn *= -1;
+                return;
+            }
+            return;
+        }
+    }
+
+    //Check if see player if see speed up, if not back to patrol speed
+    private void SeePlayer()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = new Vector2(turn*5, 0);
+        float distance = 5.0f;
+        Debug.DrawRay(position, direction, Color.blue);
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, playerLayer);
+        if (hit.collider != null)
+        {
+            if (hit.collider.name == "Player") { 
+                Debug.Log("Widze Cie!!");
+                currentSpeed = rageSpeed;
+            }
+            else
+            {
+                Debug.Log("Gdzie jestes?!!?!");
+                currentSpeed = defaultSpeed;
+            }
+        }
+        return;
     }
 
 
